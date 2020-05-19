ce3c5055771326e658c6b9d9f6828c5675b328e6
Krystian Gaczyński <30621967+krygacz@users.noreply.github.com>
2020-05-19 02:02:14 +0200

    Update example sketch

--- examples/eISCP_Send_Example/eISCP_Send_Example.ino
+++ examples/eISCP_Send_Example/eISCP_Send_Example.ino
@@ -8,6 +8,7 @@ int device_port       = 60128;    // 60128 for Onkyo A/V receivers
 
 WiFiClient client;
 eISCP device(device_ip, device_port, &client);
+void my_function(eISCP_Message message);
 
 void setup() {
   // Connect to a WiFi network
@@ -21,21 +22,39 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("\nConnected!");
+
+  // For more commands check out https://github.com/miracle2k/onkyo-eiscp/blob/master/eiscp-commands.yaml
+  String command = "MVLUP";
+  
+  // We can send messages by just passing a string to the send() method:
+  device.send(command);
+  
+  // Or create an eISCP_Message instance first...
+  eISCP_Message my_message;
+  my_message.content = command;
+  device.send(my_message);
+  
+  // ...so that we can later check the status of our message:
+  if(my_message.status == eISCP_MESSAGE_PENDING)
+    Serial.println("Command pending");
+
+  // Let's set a callback so that we can receive messages back from our device
+  // Here you can pass any method that takes eISCP_Message as argument, like my_function
+  device.set_callback(my_function);
 }
 
 void loop() {
-  // Command to be sent - eg. 'MVLUP' increments the volume on ONKYO receivers
-  // For more commands check out https://github.com/miracle2k/onkyo-eiscp/blob/master/eiscp-commands.yaml
-  String command = "MVLUP"; 
-  
-  if(device.send(command) == eISCP_SUCCESS){
-    // Command was sent successfully
-    Serial.println("Command sent!");
-  } else {
-    // There was an error
-    Serial.println("Error!");
+  // Process eISCP tasks
+  // The library won't work without this line!
+  device.handle();
+}
+
+void my_function(eISCP_Message message){
+  if(message.status == eISCP_MESSAGE_INVALID){
+    Serial.println("Message invalid: " + message.content);
+    // The message is probably corrupted
+    return;
   }
-  
-  // Wait 60s
-  delay(60000);
+  // Do something with the received message
+  Serial.println("Message received: " + message.content);
 }
