72a6b8674df45b590f5c7eb98bee7e9bdf272258
Krystian Gaczyński <30621967+krygacz@users.noreply.github.com>
2020-05-21 20:45:14 +0200

    Update example

--- examples/eISCP_Example/eISCP_Example.ino
+++ examples/eISCP_Example/eISCP_Example.ino
@@ -16,6 +16,9 @@ int device_port       = 60128;    // 60128 for Onkyo A/V receivers
 
 WiFiClient client;
 eISCP device(device_ip, device_port, &client);
+// or eISCP device(device_ip, device_port, &client, true) to enable batch processing
+// If batch processing is enabled, outgoing messages are put in a queue and sent on device.handle()
+
 void my_function(eISCP_Message message);
 
 void setup() {
@@ -31,6 +34,13 @@ void setup() {
   }
   Serial.println("\nConnected!");
 
+  // First, check if we're connected to the device
+  // This function will try to reconnect before returning false
+  if(device.connected())
+    Serial.println("Device is connected!");
+  else 
+    Serial.println("Device is not connected");
+
   // For more commands check out https://github.com/miracle2k/onkyo-eiscp/blob/master/eiscp-commands.yaml
   String command = "MVLUP";
   
@@ -38,13 +48,22 @@ void setup() {
   device.send(command);
 
   // Or create an eISCP_Message instance first...
-  eISCP_Message* my_message = new eISCP_Message;
-  my_message->content = command;
+  eISCP_Message* my_message = new eISCP_Message(command);
   device.send(my_message);
   
-  // ...so that we can later check the status of our message:
-  if(my_message->status == eISCP_MESSAGE_PENDING)
-    Serial.println("Command pending");
+  // ...so that we can later check the status of our message.
+  // This is especially useful if batch processing is enabled
+  switch(my_message->status){
+    case eISCP_MESSAGE_PENDING:
+      Serial.println("Message is waiting to be sent");
+      break;
+    case eISCP_MESSAGE_SENT:
+      Serial.println("Message sent");
+      break;
+    case eISCP_MESSAGE_NOT_SENT:
+      Serial.println("There was an error sending the message");
+      break;
+  }
 
   // Let's set a callback so that we can receive messages back from our device
   // Here you can pass any method that takes eISCP_Message as argument, like my_function
@@ -57,11 +76,15 @@ void loop() {
   device.handle();
 }
 
-void my_function(eISCP_Message message){
-  if(message.status == eISCP_MESSAGE_INVALID){
-    // The message is probably corrupted
-    return;
+void my_function(eISCP_Message* message){
+  switch(message->status){
+    case eISCP_MESSAGE_RECEIVED:
+      // Do something with the received message
+      Serial.println("Message received: " + message->content);
+      break;
+    case eISCP_MESSAGE_INVALID:
+      // The message is probably corrupted
+      Serial.println("Message corrupted: " + message->content);
+      break;
   }
-  // Do something with the received message
-  Serial.println("Message received: " + message.content);
 }
